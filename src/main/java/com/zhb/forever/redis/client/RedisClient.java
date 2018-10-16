89273f4e3c8af3b6d07e1d42e55a3fcfe11816a3
ZHB-JOB <zhb20111503@126.com>
2018-10-16 10:13:03 +0800

    redis bean 整理到xml中

--- src/main/java/com/zhb/forever/redis/client/RedisClient.java
+++ src/main/java/com/zhb/forever/redis/client/RedisClient.java
@@ -1,49 +1,73 @@
 package com.zhb.forever.redis.client;
 
 import java.util.List;
-import java.util.Set;
+import java.util.Map;
 
 public interface RedisClient {
     
+  //单个对象---------------------------------------------------------------
     /**
      * *添加，key，value
-     * @param key,value
+     * @param String,String
      */
-    void set(String key,Object value);
-    
-    /**
-     * *获取
-     * @param key
-     * @return
-     */
-    Object get(String key);
-    
-    /**
-     * *添加List
-     * @param key，List
-     * @return
-     */
-    void addList(String key,List<?> value);
-    
-    /**
-     * *获取List
-     * @param key
-     * @return
-     */
-    List<?> getList(String key);
-    
-    /**
-     * *添加Set
-     * @param key，Set
-     * @return
-     */
-    void addSet(String key,Set<?> value);
-    
-    /**
-     * *获取Set
-     * @param key
-     * @return
-     */
-    Set<?> getSet(String key);
-    
+     void set(String key,String value) ;
+   
+     /**
+      * *获取
+      * @param String
+      * @return String
+      */
+     String get(String key) ;
+   
+     /**
+      * *删除
+      * @param String
+      */
+     void del(String key) ;
+     
+   
+     /**
+      * *添加，key，value
+      * @param byte[] ,byte[]
+      */
+     void set(byte[] key,byte[] value) ;
+   
+     /**
+      * *获取
+      * @param byte[] 
+      * @return  byte[] 
+      */
+     byte[] get(byte[] key) ;
+   
+     /**
+      * *删除
+      * @param byte[]
+      */
+     void del(byte[] key) ;
+   
+
+//List---------------------------------------------------------------    
+     
+     /**
+      * *添加List
+      * @param key，List
+      * @return
+      */
+     void setList(String key,List<String> value) ;
+   
+     /**
+      * *获取List
+      * @param key
+      * @return
+      */
+     List<String> getList(String key) ;
+   
+     void delList(String key) ;
+     
+     
+//map---------------------------------------------------------------
+     void setMap(String key,Map<String, String> map) ;
+   
+     List<String> getMap(String key) ;
+
 }
--- src/main/java/com/zhb/forever/redis/client/impl/RedisClientImpl.java
+++ src/main/java/com/zhb/forever/redis/client/impl/RedisClientImpl.java
@@ -1,42 +1,113 @@
 package com.zhb.forever.redis.client.impl;
 
 import java.util.List;
-import java.util.Set;
+import java.util.Map;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.zhb.forever.framework.util.StringUtil;
 import com.zhb.forever.redis.client.RedisClient;
-import com.zhb.forever.redis.client.base.RedisTemplateBase;
 
-public class RedisClientImpl extends RedisTemplateBase implements RedisClient {
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisPool;
 
-    private Logger log = LoggerFactory.getLogger(RedisClientImpl.class);
+public class RedisClientImpl implements RedisClient {
+
+    private Logger logger = LoggerFactory.getLogger(RedisClientImpl.class);
     
-    public void set(String key,Object value) {
-        redisTemplateUtil.set(key, value);
+    private JedisPool jedisPool;
+    
+  //单个对象---------------------------------------------------------------
+    public  void set(String key,String value) {
+        Jedis jedis = jedisPool.getResource();
+        jedis.del(key);
+        jedis.set(key, value);
     }
     
-    public Object get(String key){
-        return redisTemplateUtil.get(key);
+    public  String get(String key) {
+        Jedis jedis = jedisPool.getResource();
+        return jedis.get(key);
     }
     
-    public void addList(String key,List<?> value){
-        redisTemplateUtil.setList(key, value);
+    public  void del(String key) {
+        Jedis jedis = jedisPool.getResource();
+        jedis.del(key);
     }
     
-    public List<?> getList(String key){
-        return redisTemplateUtil.getList(key);
+    public  void set(byte[] key,byte[] value) {
+        Jedis jedis = jedisPool.getResource();
+        jedis.del(key);
+        jedis.set(key, value);
+    }
+    
+    public  byte[] get(byte[] key) {
+        Jedis jedis = jedisPool.getResource();
+        return jedis.get(key);
+    }
+    
+    public  void del(byte[] key) {
+        Jedis jedis = jedisPool.getResource();
+        jedis.del(key);
+    }
+    
+    
+
+//List---------------------------------------------------------------    
+    public  void setList(String key,List<String> value) {
+        Jedis jedis = jedisPool.getResource();
+        jedis.del(key);
+        if (null != value) {
+            for (String string : value) {
+                jedis.lpush(key, string);
+            }
+        }
+        
+    }
+    
+    public  List<String> getList(String key) {
+        Jedis jedis = jedisPool.getResource();
+        if (StringUtil.isNotBlank(key)) {
+            return jedis.lrange(key, 0,-1);
+        }
+        return null;
+    }
+    
+    public  void delList(String key) {
+        Jedis jedis = jedisPool.getResource();
+        jedis.del(key);
+    }
+    
+//map---------------------------------------------------------------
+    public  void setMap(String key,Map<String, String> map) {
+        Jedis jedis = jedisPool.getResource();
+        jedis.hmset(key, map);
+    }
+    
+    public  List<String> getMap(String key) {
+        Jedis jedis = jedisPool.getResource();
+        return jedis.hvals(key);
     }
 
-    @Override
-    public void addSet(String key, Set<?> value) {
-        redisTemplateUtil.setSet(key, value);
+    
+    
+    public JedisPool getJedisPool() {
+        return jedisPool;
     }
 
-    @Override
-    public Set<?> getSet(String key) {
-        return redisTemplateUtil.getSet(key);
+    public void setJedisPool(JedisPool jedisPool) {
+        this.jedisPool = jedisPool;
     }
-
+    
+    
+    /***
+     * 
+     * 释放资源
+     */
+    public void closeResource(Jedis jedis) {
+            if(jedis != null) {
+                jedisPool.returnResource(jedis);
+            }
+    }
+    
 }
